cb99168cbbf4b62de2b8f6fd0d4ac4b3b26aa0f7
Lavie-Zanzuri <154608667+Lavie-Zanzuri@users.noreply.github.com>
2024-01-23 14:58:54 +0200

    Update GameOfLife.java

--- GameOfLife.java
+++ GameOfLife.java
@@ -136,7 +136,7 @@ public class GameOfLife {
 		//// Write your code here.
 	    for(int i = 0 ; i < arr.length ; i ++) {
 			for (int j = 0 ; j < arr[0].length ; j ++) {
-				System.out.printf("%3s" , arr[i][j])
+				System.out.printf("%3s" , arr[i][j]);
 			}
 			System.out.println();
 		}
